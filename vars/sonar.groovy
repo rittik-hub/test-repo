7311fd9ec807778721102a3af9e3938dfdd79ff9
rittik-hub <71030560+rittik-hub@users.noreply.github.com>
2023-03-20 12:29:52 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -13,5 +13,6 @@ def call()
             withSonarQubeEnv(installationName: 'sonar_jenkins', credentialsId: 'sonartest') {
                 sh "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectName=rittik -Dsonar.projectKey=rittik"
            }
+   }
            
  
